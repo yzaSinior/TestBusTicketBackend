e07373df3e68bd2c8dd1500e70d4034142b146fe
YOUNES ZAROUAL <younes.zaroual@ext.soprasteria.com>
2023-03-27 10:40:52 +0200

    [YZA] : Prepare configuration H2 database

---
+++ src/main/java/com/example/testbusticket/config/DatabaseConfig.java
@@ -0,0 +1,25 @@
+package com.example.testbusticket.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.example.testbusticket.repository")
+@EnableTransactionManagement
+public class DatabaseConfig {
+
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource dataSource = new DriverManagerDataSource();
+    dataSource.setDriverClassName("org.h2.Driver");
+    dataSource.setUrl("jdbc:h2:./busbookingdb");
+    dataSource.setUsername("sa");
+    dataSource.setPassword("");
+    return dataSource;
+  }
+}
